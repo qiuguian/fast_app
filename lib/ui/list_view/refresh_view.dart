d861b98b8bdb94a48bdce0ac148fa6ae4e3691fd
anan <anan@163.com>
2021-11-07 20:43:52 +0800

    优化翻页

--- lib/ui/list_view/refresh_view.dart
+++ lib/ui/list_view/refresh_view.dart
@@ -4,11 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class RefreshView extends StatefulWidget {
-  RefreshView({required this.child,this.onRefresh,this.onLoading});
+  RefreshView({required this.child, this.onRefresh, this.onLoading,this.hasNextPage});
 
   final Widget child;
-  final VoidCallback? onRefresh;
-  final VoidCallback? onLoading;
+  final Function()? onRefresh;
+  final Function()? onLoading;
+  final bool Function()? hasNextPage;
 
   @override
   _RefreshViewState createState() => _RefreshViewState();
@@ -16,13 +17,13 @@ class RefreshView extends StatefulWidget {
 
 class _RefreshViewState extends State<RefreshView> {
   RefreshController _refreshController =
-      RefreshController(initialRefresh: false);
+  RefreshController(initialRefresh: false);
 
   @override
   void initState() {
     super.initState();
 
-    FastNotification.addListener("RefreshViewFinish", (data){
+    FastNotification.addListener("RefreshViewFinish", (data) {
       _refreshController.refreshCompleted();
       _refreshController.loadComplete();
     });
@@ -43,16 +44,18 @@ class _RefreshViewState extends State<RefreshView> {
       footer: CustomFooter(
         builder: (BuildContext context, LoadStatus? mode) {
           Widget body;
-          if (mode == LoadStatus.idle) {
-            body = Text("pull up load");
+          if (mode == LoadStatus.noMore) {
+            body = Text("无更多数据");
+          } else if (mode == LoadStatus.idle) {
+            body = Text("向上拉加载更多");
           } else if (mode == LoadStatus.loading) {
             body = CupertinoActivityIndicator();
           } else if (mode == LoadStatus.failed) {
-            body = Text("Load Failed!Click retry!");
+            body = Text("加载失败!");
           } else if (mode == LoadStatus.canLoading) {
-            body = Text("release to load more");
+            body = Text("放开加载更多");
           } else {
-            body = Text("No more Data");
+            body = Text("无更多数据");
           }
           return Container(
             height: 55.0,
@@ -61,8 +64,19 @@ class _RefreshViewState extends State<RefreshView> {
         },
       ),
       controller: _refreshController,
-      onRefresh: widget.onRefresh,
-      onLoading: widget.onLoading,
+      onRefresh: () async {
+        _refreshController.resetNoData();
+        await widget.onRefresh?.call();
+        _refreshController.refreshCompleted();
+      },
+      onLoading: () async {
+        if (widget.hasNextPage?.call() ?? true) {
+          List data = await widget.onLoading?.call() ?? [];
+          _refreshController.loadComplete();
+        } else {
+          _refreshController.loadNoData();
+        }
+      },
       child: widget.child,
     );
   }
